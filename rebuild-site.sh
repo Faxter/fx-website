b89a3888e7c493eb60243aac6b91f716751b9e4c
Faxter <faxter313@gmail.com>
2024-06-18 21:28:31 +0200

    automate git pull in rebuild script

--- rebuild-site.sh
+++ rebuild-site.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
+echo -e "----- git pull fx-website \n"
+git pull
+echo -e "\n\n"
+
+echo -e "----- git pull xiv-guides \n"
+git --git-dir=../xiv-guides/.git --work-tree=../xiv-guides/ pull
+echo -e "\n\n"
+
+echo -e "----- deleting site"
 rm -rf build/site
+echo -e "\n\n"
+
+echo -e "----- generating new site"
 npx antora antora_playbook.yml --fetch
