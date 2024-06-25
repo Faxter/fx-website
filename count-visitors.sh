b6431afea000eb59623ec8778753cc9d7faaf4b0
Faxter <faxter313@gmail.com>
2024-06-25 18:39:45 +0200

    script for visitor count

---
+++ count-visitors.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# search log file for all lines containing faxxter.com
+# cut first part to first ' ' - this is the IP address
+# only keep unique addresses
+# wordcount the lines
+
+echo -e "number of unique visitors in access.log:\n"
+grep 'faxxter.com' /var/log/apache2/access.log | cut -d' ' -f1 | uniq | wc -l
